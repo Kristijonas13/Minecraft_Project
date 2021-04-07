e09d64f30aec1015773bf676b257d81295ab8d02
Kristijonas Bileisis <kristijonas13@gmail.com>
2021-04-07 17:26:22 -0400

    Made changes to region.py

--- src/region.py
+++ src/region.py
@@ -6,7 +6,7 @@
 
 #imports
 import logging
-import yaml
+import json
 import os 
 import sqlite3
 from datetime import datetime
@@ -65,9 +65,70 @@ def delete_region(delete_list, cursor):
 
 
 def insert_region(insert_list, cursor, region_path):
-    print(insert_list)
+
+    data_list = [] 
+    try: 
+        for file_path in insert_list:
+            with open(file_path) as file:
+                region_dict = json.load(file)
+
+        for table, columns in region_dict.items(): 
+            print(table)
+            for column in columns:
+                print(column)
+        ax = create_insert_query_list(['region_info', ['region_name', 'description']],region_dict)
+        query_list = create_insert_queries(ax)
+        for query in query_list: 
+            cursor.execute(query)
+        
+    except sqlite3.Error as error:
+        print("error")
 
 
+
+def create_insert_queries(insert_query_list):
+
+    queries = []
+    
+    for value in insert_query_list[2]:
+        sqlite_insert_query = 'insert into ' + str(insert_query_list[0]) + ' ('
+        for column_name in insert_query_list[1]:
+            sqlite_insert_query = sqlite_insert_query + str(column_name) + ', '
+        sqlite_insert_query= sqlite_insert_query[:-2] + ') values ('
+        for valu in value: 
+            sqlite_insert_query= sqlite_insert_query + "'"+str(valu) + "',"
+        sqlite_insert_query = sqlite_insert_query[:-1] + ');' 
+        queries.append(sqlite_insert_query)
+    return queries
+    
+    
+
+
+def create_insert_query_list(name_column,region_dict):
+
+    insert_query_list = []
+    ax = []
+    ax.append(name_column[0])
+    ax.append(name_column[1])
+    ax.append(get_insert_values(region_dict[name_column[0]], name_column[1]))
+    ax.append(len(name_column[1]))
+    return ax
+
+    #[region_table, [column1,column2], [(values),(value)], num_of_columns]
+
+    
+    values_list = get_insert_values
+
+def get_insert_values(table_name, column_names_list):
+    
+    values_list = []
+    values2_list = []
+    for name in table_name:
+        for column in column_names_list:
+            values_list.append(name[column])
+        values2_list.append(tuple(values_list))
+        values_list = []
+    return values2_list
 
 #return nested list [region_name, last_modified, filename, table_to_modify, [[old_values,new_values]],]
 def update_region(update_list, cursor):
@@ -86,10 +147,11 @@ def check_insert(region_path, cursor):
         region_list = cursor.fetchall()
 
         for region_file in region_dir:
-            insert_list.append(region_file[:-4])
+            region_file_path = str(region_path) + "\\" + str(region_file)
+            insert_list.append(region_file_path)
             for region in region_list:
                 if str(region[0]) == str(region_file[:-4]):
-                    insert_list.remove(region[0])
+                    insert_list.remove(region_file_path)
         return insert_list
 
     except sqlite3.Error as error:
@@ -98,7 +160,7 @@ def check_insert(region_path, cursor):
 
 
 def check_delete(cursor):
-    print("check")
+    return []
 
 
 
@@ -108,18 +170,10 @@ def check_update(region_path, cursor):
         update_list = []
         filename_list = check_last_modified(region_path, cursor)
 
-        for filename in filename_list: 
-            file = open(filename, 'r')
-            list = yaml.load(file, Loader=yaml.FullLoader)
-            
-
     except sqlite3.Error as error:
         print("error")
     
     
-
-
-
 
 def check_last_modified(region_path, cursor):
 
